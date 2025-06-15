85f80389b1622efcf3f0eeb89bc4510591487d4d
PauloCesar26 <pc.cordeirolima@gmail.com>
2025-06-15 19:42:05 -0300

    Projeto finalizado de regex

--- FreeCodeCamp-Studying/JavaScript/JS12_RegularExpressions/script.js
+++ FreeCodeCamp-Studying/JavaScript/JS12_RegularExpressions/script.js
@@ -1,6 +1,7 @@
 const messageInput = document.getElementById("message-input");
 const result = document.getElementById("result");
 const checkMessageButton = document.getElementById("check-message-btn");
+
 //Expressão regular (regex)
     //Elas podem receber sinalizadores para modificar seu comportamento
     //sinalidazor i: pode ser usado para fazer com que a expressão ignore 
@@ -9,10 +10,33 @@ const checkMessageButton = document.getElementById("check-message-btn");
 const helpRegex = /please help|assist me/i;
 //expressão regular que corresponde a menções de valores em dolares
     //usando a classe de caracteres é definida por [] e corresponde a qualquer caracter dentro dele
-    //Por exemplo, [a-z] corresponde a qualquer caractere de a a z.
-const dollarRegex = /[0-9] dollars/i;
+        //Por exemplo, [a-z] corresponde a qualquer caractere de a a z.
+    //quantificador + diz que pode ter um ou mais num consecutivos, como o valor em dolar que pode ter mais de um digito
+        //por exemplo a regular expression /a+/ mostra que pode ter um ou mais caracteres a consecutivos
+    //() define um grupo de captura
+        //por exemplo /h(i/ey) camper/ combinaria com qualquer um:hi camper ou hey camper, capturaria i ou ey de um grupo
+    //? mostra que é algo opcional
+        //for example o regex /colou?r/ corresponde tanto a color e colour porque o "u" é opcional 
+    //\s corresponde a espaços em branco, como espaços, tubulações e novas linhas
+        //* siginifa que corresponde ao caractere anterior pode estar presente 0 ou mais vezes
+            //for example /ab*c/ corresponde a ac(zero ocorrecnia de b), abc(uma ocorrencia de b)
+    //nesse regex não precisa do valor do grupo de captura, trnsformar ele em um grupo sem captura primite que agrupe os
+        //caractres sem preservar o resultado, para criar o grupo sem captura pode add "?:" depois dos parenteses
+    //?: define um grupo de captura não capturante, as coisas dentro dos () é agrupado para fazer operações(|, *, +)
+        //mas o conteudo correspondido não sera armazenado como parte dos resultados capturados
+const dollarRegex = /[0-9]+\s*(?:hundred|thousand|million|billion)?\s+dollars/i;
+//regex que vai procurar por strings como "free money"
+    //msg spam normalmente utilizam num em vez de letras para contornar os filtros, o regex vai capturá-los
+        //colocando classes[] de caracteres que corresponde a "e" e "3"
+    //para o regex corresponder ao inicio do texto, tem que usar a ancora "^", que garante que começa no incio da string completa
+        //para corresponder ate o final da string é usado a ancora "$"
+const freeRegex = /(?:^|\s)fr[e3][e3] m[o0]n[e3]y(?:$|\s)/i;
 
-const denyList = [helpRegex, dollarRegex];
+const stockRegex = /(?:^|\s)[s5][t7][o0][c{[(]k [a@4]l[e3]r[t7](?:$|\s)/i;
+
+const dearRegex = /(?:^|\s)d[e3][a@4]r fr[i1|][e3]nd(?:$|\s)/i;
+
+const denyList = [helpRegex, dollarRegex, freeRegex, stockRegex, dearRegex];
 //Função está return implicitamente
 //.match() metodo das strings, que aceita uma expressão regex(regular) como
     //argumento e determina se a string corresponde a essa expressão 
