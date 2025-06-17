3daeca803ef2116bd5af8e282ffa05ae9bb24fd2
Paulo Cesar Cordeiro Lima <paulo.lima@sindiabc.local>
2025-06-17 15:38:43 -0300

    Saving

--- FreeCodeCamp-Studying/JavaScript/JS13_NumberSorter/script.js
+++ FreeCodeCamp-Studying/JavaScript/JS13_NumberSorter/script.js
@@ -1,5 +1,5 @@
 const sortButton = document.getElementById("sort");
-//Ouvinte de eventos para o sortButton
+
 const sortInputArray = (event) => {
     event.preventDefault();
     //catch them all have the class "values-dropdown"
@@ -9,15 +9,34 @@ const sortInputArray = (event) => {
     //thse values select will currently be strings and will convert them into numbers
         //use the map function to iterate over the array
         //funtion return implicit
-    const inputValues = [...document.getElementsByClassName("values-dropdown")].map(dropdown =>  dropdown.value);
+    //function Number() convert strings for number, return array of number now
+    const inputValues = [...document.getElementsByClassName("values-dropdown")].map(dropdown =>  Number(dropdown.value));
+    updateUI(inputValues);
+};
+//function to update display with the sorted numbers 
+    //that takes a single array parameter
+    //writing algorithms that won't immediately have a return value, set a fallback value for array to be an empty array
+const updateUI = (array = []) => {
+    array.forEach((num, i) => {
+        const outputValueNode = document.getElementById(`output-value-${i}`);
+        outputValueNode.innerText = num;
+    });
+};
 
-
-    
-    
-
-
-
-
+const bubbleSort = (array) => {
+    //for to iterate through the array 
+        //compare elements need nasted for loop
+    for(let i = 0; i < array.length; i++){
+        //iterate all values except the last one
+        for(let j = 0; j < array.length - 1; j++){
+            console.log(array, array[j], array[j + 1]);
+        }
+    }
 };
 
 sortButton.addEventListener("click", sortInputArray);
+
+//por que sou importante aqui?
+//o que posso fazer para ser importante aqui? 
+//como fazer a diferença?
+//como chamar a atenção do chefe?
