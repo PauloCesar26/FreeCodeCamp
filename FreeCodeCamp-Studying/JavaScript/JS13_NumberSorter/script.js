6761340fc48a7131c06539f23cdb0c2207c478ce
Paulo Cesar Cordeiro Lima <paulo.lima@sindiabc.local>
2025-07-03 15:15:45 -0300

    Finished project about method sort

--- FreeCodeCamp-Studying/JavaScript/JS13_NumberSorter/script.js
+++ FreeCodeCamp-Studying/JavaScript/JS13_NumberSorter/script.js
@@ -11,7 +11,11 @@ const sortInputArray = (event) => {
         //funtion return implicit
     //function Number() convert strings for number, return array of number now
     const inputValues = [...document.getElementsByClassName("values-dropdown")].map(dropdown =>  Number(dropdown.value));
-    const sortedValues = selectionSort(inputValues);
+    //method .sort() the default behavior of sort is to convert the numbes values to strings and sort them alphabetically
+    //a and b represent the number values in the array that will be sorted
+    const sortedValues = inputValues.sort((a, b) => {
+        return a - b;
+    });
     updateUI(sortedValues);
 };
 //function to update display with the sorted numbers 
@@ -49,13 +53,42 @@ const bubbleSort = (array) => {
     //it continues iteratingthrough the array until it is completely sorted 
 const selectionSort = (array) => {
     for(let i = 0; i < array.length; i++){
+        //A selection sort relies on tracking the index of the smallest value in the array
+            //variable minIndex ensures that if your current value is the smallest, it will be  swapped with itself and not be moved
+        let minIndex = i;
+        for(let j = i + 1; j < array.length; j++){
+            console.log(array, array[j], array[minIndex]);
 
+            if(array[j] < array[minIndex]){
+                minIndex = j;    
+            }    
+        }
+        const temp = array[i];
+        array[i] = array[minIndex];
+        array[minIndex] = temp;
+    }
+    return array;
+};
+
+//Insertion sort
+    //Algorithm works by building up a sorted array at the beginning of the list
+    //It begins the sorted array with the fist element
+    //Then it inspects the next element and swaps it backward into the sorted array until it is in a sorted position, and so on
+const insertionSort = (array) => {
+    for(let i = 1; i < array.length; i++){
+        const currValue = array[i];
+        let j = i - 1;
+
+        while(j >= 0 && array[j] > currValue){
+            //On each iteration of your while loop it is finding an element that is larger than your current value
+                //Need to moe that element to the rigth to make room for your current value
+            array[j + 1] = array[j];
+            j--;
+        }
+        //Insert current value
+        array[j + 1] = currValue;
     }
 }
 
 sortButton.addEventListener("click", sortInputArray);
 
-//por que sou importante aqui?
-//o que posso fazer para ser importante aqui? 
-//como fazer a diferença?
-//como chamar a atenção do chefe?
