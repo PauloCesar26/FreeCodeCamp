d2e11c772c27fbcd38e7c4e4e2db4ab8d9495ac2
Paulo Cesar Cordeiro Lima <paulo.lima@sindiabc.local>
2025-08-13 15:22:01 -0300

    Save project, finished

--- FreeCodeCamp-Studying/JavaScript/JS15_BuildingSpreadsheet/script.js
+++ FreeCodeCamp-Studying/JavaScript/JS15_BuildingSpreadsheet/script.js
@@ -4,7 +4,14 @@ const infixToFunction = {
     "*": (x, y) => x * y,
     "/": (x, y) => x / y
 };
-const infixEval = (str, regex) => str.replace(regex, (_match, arg1, operator, arg2) => {});
+const infixEval = (str, regex) => str.replace(regex, (_match, arg1, operator, arg2) => infixToFunction[operator](parseFloat(arg1), parseFloat(arg2)));
+
+const highPrecedence = (str) => {
+    const regex = /([\d.]+)([*\/])([\d.]+)/;
+    const str2 = infixEval(str, regex);
+    return str2 === str ? str : highPrecedence(str2);
+};
+
 const isEven = (num) => num % 2 === 0 ? true : false; 
 const sum = (nums) => nums.reduce((acc, el) => acc + el, 0);
 const average = (nums) => sum(nums) / nums.length; 
@@ -22,11 +29,36 @@ const median = (nums) => {
 };
 
 const spreadsheetFunctions = {
+    "": nums => nums,
     sum,
     average,
     median,
+    even: nums => nums.filter(isEven),
+    someeven: nums => nums.some(isEven),
+    everyeven: nums => nums.every(isEven),
+    firsttwo: nums => nums.slice(0, 2),
+    lasttwo: nums => nums.slice(-2),
+    has2: nums => nums.includes(2),
+    increment: nums => nums.map(num => num + 1), 
+    random: ([x, y]) => Math.floor(Math.random() * y + x), 
+    range: nums => range(...nums),
+    nodupes: nums => nums.filter((item, index) => nums.indexOf(item) === index)
 };
 
+const applyFunction = (str) => {
+    const noHigh = highPrecedence(str);
+    const infix = /([\d.]+)([+-])([\d.]+)/;
+
+    const str2 = infixEval(noHigh, infix);
+    const functionCall = /([a-z0-9]*)\(([0-9., ]*)\)(?!.*\()/i;
+
+    const toNumberList = (args) => args.split(",").map(parseFloat);
+    const apply = (fn, args) => spreadsheetFunctions[fn.toLowerCase()](toNumberList(args));
+    //.hasOwnProperty() check if a property on a given objects exists or not, method returns true or false 
+        //depending on if the property is found on the object or not
+    return str2.replace(functionCall, (match, fn, args) => spreadsheetFunctions.hasOwnProperty(fn.toLowerCase()) ? apply(fn, args) : match); 
+}
+ 
 //function to generate a rage of numbers
     //Array need to be the size of the range, calculate this by finding the difference between end and start and add 1 to the result
     //.fill() method which can be used to fill an array with a value
@@ -60,6 +92,9 @@ const evalFormula = (x, cells) => {
     const rangeExpanded = x.replace(rangeRegex, (_match, char1, num1, char2, num2) => rangeFromString(num1, num2).map(addCharacters(char1)(char2)));
     const cellRegex = /[A-J][1-9][0-9]?/gi;
     const cellExpanded = rangeExpanded.replace(cellRegex, (match) => idToText(match.toUpperCase()));
+    const functionExpanded = applyFunction(cellExpanded);
+
+    return functionExpanded === x ? functionExpanded : evalFormula(functionExpanded, cells);
 }
 
 window.onload = () => {
@@ -95,6 +130,7 @@ const update = (event) => {
     const value = element.value.replace(/\s/g, "");
     //.charAt method is used to retrieve a specific character from a string based on its index(position)    
     if(!value.includes(element.id) && value.charAt(0) === "="){
-
+        //Array.from() to convert it to an array
+        element.value = evalFormula(value.slice(1), Array.from(document.getElementById("container").children));
     }
 }
